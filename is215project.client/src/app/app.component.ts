0c659dfa28cce64988ca7fc5d1a32de81cf65e77
Michael Episcope <mjepiscope@gmail.com>
2024-05-13 12:18:40 +0800

    Added basic image adjust. Yet to apply KC's responsive adjust image function.

--- is215project.client/src/app/app.component.ts
+++ is215project.client/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  imageSrc!: string;
+
+  onImageChange(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      this.imageSrc = URL.createObjectURL(event.target.files[0]);
+    }
+  }
+
 }
